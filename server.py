3e31ff66234dc8d47b024c2a9a6b6c765e6e0af3
Aarav Dave <aarinaarav@gmail.com>
2021-02-14 19:53:21 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,6 @@ import socket
 
 def connect(site):
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-      server.connect((f'{site}.com' , 80))
-      server.sendall(f'GET / HTTP/1.1\r\nHost: {site}.com\r\nAccept: text/html\r\n\r\n'.encode('UTF-8'))
+      server.connect((f'{site}' , 80))
+      server.sendall(f'GET / HTTP/1.1\r\nHost: {site}\r\nAccept: text/html\r\n\r\n'.encode('UTF-8'))
       print(str(server.recv(4096), 'utf-8'))
