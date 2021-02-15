97f8f446d748e585009705c624567051202041f1
Aarav Dave <aarinaarav@gmail.com>
2021-02-14 19:51:32 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,8 @@
 import socket
+
+
+def connect(site):
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+      server.connect((f'{site}.com' , 80))
+      server.sendall(f'GET / HTTP/1.1\r\nHost: {site}.com\r\nAccept: text/html\r\n\r\n'.encode('UTF-8'))
+      print(str(server.recv(4096), 'utf-8'))
